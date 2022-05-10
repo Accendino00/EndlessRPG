c339966ea5efae7b8c15c7f1ac900b50107b0b89
Accendino <other2180@gmail.com>
2022-05-10 23:38:25 +0200

    Player commit

--- src_2/Player.cpp
+++ src_2/Player.cpp
@@ -3,40 +3,52 @@
 
 Player::Player(int y, int x):Entita(y, x, 0, 0, NULL){
 
-    // ricorda di chiudere queste cose dopo che le apri
     this->stampabile = new cchar_t * [1];
     this->stampabile[0] = new cchar_t [1];
-
-    srand(time(NULL));
 
     wchar_t dragonerosso [5] = L"\u2665";
    
     setcchar(&(this->stampabile[0][0]), dragonerosso, A_BOLD, 2, NULL);
-    
+
     this->y = y;
     this->x = x;
 
 };
 
-void Player::movimento(int input, Stanza stanza){
-	
+void Player::movimento(int input, Stanza stanza)
+{
+	int lastinput = NULL;
+
     switch(input) {
 			case (L'w'):
-				y--;
-				break;
+			    y--;
+                lastinput = (L'w');
+			    break;
 			case (L's'):
 				y++;
+                lastinput = (L's');
 				break;
 			case (L'd'):
 				x++;
+                lastinput = (L'd');
 				break;
 			case (L'a'):
 				x--;
+                lastinput = (L'a');
 				break;
+            case (L' '):
+                if(lastinput == NULL) y=y-2;
+                if(lastinput == L'w') y=y-2;
+                if(lastinput == L's') y=y+2;
+                if(lastinput == L'd') y=x-2;
+                if(lastinput == L'a') y=x+2;
+                break;
+
 		}
 
 };
 
-void inventario(){
+void Player::inventario(){
+    getmaxyx(stdscr, yMax, xMax);
 
 };
