4428ac6ab3614a192cca65c76baa0ae7afaa4856
jacopoamoretti <98223896+jacopoamoretti@users.noreply.github.com>
2022-09-04 15:50:46 +0200

    Aggiornamento corretto di MenuScore

--- src_2/menu/MenuScore.cpp
+++ src_2/menu/MenuScore.cpp
@@ -1,1 +1,81 @@
 #include "../generale/libs.hpp"
+
+MenuScore::MenuScore() : Menu(0,1,8) {}
+
+void GameData::caricaScore() {
+    ptrlist tmp = this->head; //Salvo la testa
+    FILE *fin;
+    fin = fopen("score.csv", "r");
+    //booleano per uscire dalla lettura
+    bool exit=true;
+    // Temp per la lettura
+    char c;
+    while(exit) {
+        if (fin == NULL) {
+            char str[50] = "Il file score è inesistente"
+            printf("%s",str);
+        } else {
+            ptrlist curr = new dati;
+            while (c != ';') {   //Carico il nome
+                c = fgetc(fin);
+                strcpy(curr->nome[i], c);
+                i++;
+            }
+            curr->score = 0;     //Carico il punteggio
+            int i = 10000000;
+            while (c != '\n') {
+                c = fgetc(fin);
+                if (c != '\n') {
+                    curr->score += (c - '0') * i;
+                    i /= 10;
+                }
+            }
+            while (i >= 1) {
+                curr->score = curr->score / 10;
+                i /= 10;
+            }
+            curr->next = tmp;
+        }
+        //Controllo per fine del file
+        if (feof(fin)) {
+            exit = false;
+        }
+    }
+}
+
+void MenuScore::loopScore() {
+    bool esciDaScore = false;
+    do {
+        gd->frameStart();
+        gd->getInput();
+        erase();
+        this->manageInput();
+        int i = 0;
+        while(i < gd->getNumOfPressedKeys()) {
+            switch(gd->getKey(i)) {
+                case KEY_LEFT: //Mi muovo a sinistra nella pagina
+                case L'A':
+                case L'a':
+
+
+                    }
+                    break;
+                case KEY_RIGHT: //Mi muovo a destra nella pagina
+                case L'D':
+                case L'd':
+
+                    }
+                    break;
+            }
+            i++;
+        // Stampa, fine del frame e refresh dello schermo
+        this->printAll();
+        gd->frameFinish();
+        refresh();
+        switch()
+
+        while (!esciDaScore)
+    }
+}
+
+
--- src_2/menu/MenuScore.hpp
+++ src_2/menu/MenuScore.hpp
@@ -1,34 +1,53 @@
 #pragma once
 #include "../generale/libs.hpp"
 
+//Dato generico per lo score (punteggio e nome di 3 caratteri)
+struct dati {
+    int score;
+    char [3] nome;
+    dati *next;
+};
+typedef dati *ptrlist;
+
 class MenuScore : public Menu {
-    /*
-    idea di dato:
-    struct dati {
-        int score;
-        char [3] nome;
-    } 
-    dati [100] da_leggere;
-    (^^^^ meglio da implementare dinamicamente ^^^^)
-    (^^^^ idea: radoppia la dimensione dell'array se hai finito lo spazio ^^^^)
-
-    non usare malloc e free, usa solo new e delete
-
-    leggere tutti i dati da un file csv del tipo:
-        AAA;1231512
-        BAS;512
-        ...
-        ZZZ;323357
-
-    ordinare i dati letti in ordine decrescente
-
-    stamparne 10 a volta sullo schermo, poi avere un opzione per cambiare pagina
-    e mostrare i 10 dopo, e così via
-
-
-    ricordati di deallocare tutto con il destrucotor alla fine (metodo scritto come: ~MenuScore())
-    */
-
+private:
+    ptrlist head;     //Testa della lista dei punteggi
 public:
     MenuScore();
+    void caricaScore();
+    void loopScore();
+    void printAll();
+
 };
+
+/*
+idea di dato:
+struct dati {
+    int score;
+    char [3] nome;
+}
+dati [100] da_leggere;
+(^^^^ meglio da implementare dinamicamente ^^^^)
+(^^^^ idea: radoppia la dimensione dell'array se hai finito lo spazio ^^^^)
+
+non usare malloc e free, usa solo new e delete
+
+leggere tutti i dati da un file csv del tipo:
+    AAA;1231512
+    BAS;512
+    ...
+    ZZZ;323357
+
+ordinare i dati letti in ordine decrescente
+
+stamparne 10 a volta sullo schermo, poi avere un opzione per cambiare pagina
+e mostrare i 10 dopo, e così via
+
+
+ricordati di deallocare tutto con il destructor alla fine (metodo scritto come: ~MenuScore())
+
+ I valori che mostri, dallarray di valori letti, sono da
+Arr[numpagina*10]
+Fino a
+Arr[(numpagina+1)*10]
+*/
